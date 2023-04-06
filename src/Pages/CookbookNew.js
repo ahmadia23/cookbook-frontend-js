6047b1bc4c4e1f4c4dcbd65102a9852c5f44be46
ahmadou dia <dia.ahmadou23@gmail.com>
2023-04-06 22:38:29 +0000

    UPDATE: File upload

--- src/Pages/CookbookNew.js
+++ src/Pages/CookbookNew.js
@@ -30,7 +30,6 @@ export const action = async ({ request, params }) => {
     {
       method: "POST",
       headers: {
-        "Content-Type": "application/json",
         Authorization: "Bearer " + token,
       },
       body: JSON.stringify(cookbookData),
--- src/components/forms/CookbookForm.js
+++ src/components/forms/CookbookForm.js
@@ -2,11 +2,16 @@ import React, { Fragment } from "react";
 import "./CookbookForm.css";
 import "../../Pages/Login.css";
 import { Form } from "react-router-dom";
-import { ImagePicker } from "react-file-picker";
 
 const CookbookForm = (props) => {
   const handleSubmit = (event) => {
     console.log("I submited the form");
+  };
+
+  const fileNameHandler = (e) => {
+    console.log(e.target.value);
+    const fileName = document.getElementById("file-selected");
+    fileName.innerText = e.target.value.split("\\").slice(-1);
   };
 
   return (
@@ -17,8 +22,8 @@ const CookbookForm = (props) => {
         alt="create a cookbook"
       ></img>
       <Form onSubmit={handleSubmit} method="post" className="cookbook-form">
-        <h2 class="title">Create your Cookbook</h2>
-        <h3 class="subtitle">Connect with the community!</h3>
+        <h2 className="title">Create your Cookbook</h2>
+        <h3 className="subtitle">Connect with the community!</h3>
         <div className={"input-container ic1"}>
           <input
             type="text"
@@ -27,7 +32,7 @@ const CookbookForm = (props) => {
             placeholder="My wonderful asian rolls..."
             value={props.nameValue}
           ></input>
-          <div class="cut"></div>
+          <div className="cut"></div>
           <label className="placeholder">Cookbook Name</label>
         </div>
         <div className={"input-container ic2"}>
@@ -38,7 +43,7 @@ const CookbookForm = (props) => {
             placeholder="This cookbook has a variety of..."
             value={props.descriptionValue}
           ></input>
-          <div class="cut cut-short"></div>
+          <div className="cut cut-short"></div>
           <label className="placeholder">Description</label>
         </div>
         <div className={"input-container ic1"}>
@@ -49,26 +54,20 @@ const CookbookForm = (props) => {
             placeholder="Asian food.."
             value={props.themeValue}
           ></input>
-          <div class="cut cut-short"></div>
+          <div className="cut cut-short"></div>
           <label className="placeholder">Theme</label>
         </div>
         <div className={"input-container ic1"}>
-          <ImagePicker
-            extensions={["jpg", "jpeg", "png"]}
-            dims={{
-              minWidth: 100,
-              maxWidth: 500,
-              minHeight: 100,
-              maxHeight: 500,
-            }}
-            onChange={(base64) => {}}
-            onError={(errMsg) => {}}
-            required
-          >
-            <button className="button__picker">Click to upload image</button>
-          </ImagePicker>
-          <div class="cut cut-short"></div>
-          <label className="placeholder">Image Url</label>
+          <label className="custom-file-upload" htmlFor="file-upload">
+            Upload an image
+          </label>
+          <span id="file-selected"></span>
+          <input
+            type="file"
+            name="image"
+            id="file-upload"
+            onChange={fileNameHandler}
+          ></input>
         </div>
         <button type="submit" className="submit">
           Create Cookbook
