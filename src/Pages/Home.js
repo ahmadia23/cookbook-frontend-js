eaf1ae246776a50e0c806142f1ce314495524228
ahmadou dia <dia.ahmadou23@gmail.com>
2023-04-14 23:18:54 +0000

    ADD: responsive home 2/2

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -19,33 +19,37 @@ const Home = () => {
   return (
     <Fragment>
       <Banner></Banner>
-      <div className="home-content">
-        <PromessCard
-          src2={homePng}
-          src1={
-            "https://cdn.pixabay.com/photo/2017/08/08/08/57/cake-2610754_1280.jpg"
-          }
-        />
-      </div>
-      <p className="home-website reveal">
-        We've made it easy for you to discover new recipes, organize your
-        favorite dishes, and share your culinary creations with friends and
-        family.
-      </p>
-      <img
-        className="image-website reveal"
-        src={websitePng}
-        alt="cooking website"
-      ></img>
-      <p className="home-community reveal">
-        Join our community of passionate cooks and start creating your own
-        cookbook today.
-      </p>
-      <div className="home-actions reveal">
-        <p>Unleash your culinary creativity and let's get cooking!</p>
-        <button className="home-link" onClick={launchAppHandler}>
-          Launch App
-        </button>
+      <div className="home-container">
+        <div className="home-content">
+          <PromessCard
+            src2={homePng}
+            src1={
+              "https://cdn.pixabay.com/photo/2017/08/08/08/57/cake-2610754_1280.jpg"
+            }
+          />
+        </div>
+        <div className="home-website reveal">
+          <p>
+            We've made it easy for you to discover new recipes, organize your
+            favorite dishes, and share your culinary creations with friends and
+            family.
+          </p>
+          <img
+            className="image-website"
+            src={websitePng}
+            alt="cooking website"
+          ></img>
+        </div>
+        <p className="home-community reveal">
+          Join our passionated community and start creating your own cookbook
+          today.
+        </p>
+        <div className="home-actions reveal">
+          <p>Unleash your culinary creativity and let's get cooking!</p>
+          <button className="home-link" onClick={launchAppHandler}>
+            Launch App
+          </button>
+        </div>
       </div>
     </Fragment>
   );
